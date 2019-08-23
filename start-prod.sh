c231fba5df42885bc2972bdbf903a8b6085a9dd4
Tmlh <tmlh98@aliyun.com>
2019-08-23 10:53:07 +0800

    .

--- start-prod.sh
+++ start-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo 'staring forum'
 
-nohup java -jar -Dspring.profiles.active=prod /root/java-app/spring-monkey/tmlh-forum-web/target/forum.jar &
+nohup java -jar -Dspring.profiles.active=prod /home/www/tmlh.xyz/spring-monkey/tmlh-forum-web/target/forum.jar &
 echo 'Start Success!'
